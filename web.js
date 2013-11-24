4b96f47f406263fef68e8b624ed290c68a0911d5
Souvik Banerjee <souvik1997@gmail.com>
2013-11-23 19:44:44 -0600

    Per-question time limits. Change placeholder to something more reasonable

--- web.js
+++ web.js
@@ -19,8 +19,8 @@ app.listen(port, function() {
 
 function getData()
 {
-	data = [{type:"mathjax",data:"\\(\\frac{\\mathrm{d} y} {\\mathrm{d} x}\\int_{0}^{\\pi } x^{\\sqrt{x^{\\frac{2}{3}}{}}}\\)"}, /* \frac{3}{2} */
-			{type:"image", data:"http://placehold.it/850x150"}];
+	data = [{type:"mathjax",data:"\\(\\frac{\\mathrm{d} y} {\\mathrm{d} x}\\int_{0}^{\\pi } x^{\\sqrt{x^{\\frac{2}{3}}{}}}\\)",time:10}, /* \frac{3}{2} */
+			{type:"image", data:"http://placehold.it/650x100",time:30}];
 	num = data.length;
 	return { indata : data, numSlides:num };
 }
