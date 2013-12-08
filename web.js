277590806460cd07665cdfbc67a44760b4e7bdde
Souvik Banerjee <souvik1997@gmail.com>
2013-12-08 10:57:21 -0600

    Basic cookie+hash security

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 var port = process.env.PORT || 8080;
 app.use(express.bodyParser());
+app.use(express.cookieParser())
 var MongoClient = require('mongodb').MongoClient;
 app.locals.pretty = true;
 
@@ -14,7 +15,16 @@ app.use("/static",express.static(__dirname+"/static"));
 app.listen(port, function() {                           
   console.log("Listening on " + port);
 });
-
+String.prototype.hashCode = function(){
+				var hash = 0;
+				if (this.length == 0) return hash;
+				for (i = 0; i < this.length; i++) {
+					char = this.charCodeAt(i);
+					hash = ((hash<<5)-hash)+char;
+					hash = hash & hash; // Convert to 32bit integer
+				}
+				return hash;
+			}
 function getData()
 {
 	data = [{
@@ -41,31 +51,37 @@ MongoClient.connect('mongodb://'+process.env.MONGOHQUSERNAME+':'+process.env.MON
 
 		app.routes.get = []; //Clear routes
 		app.get("/:name",function(request,response)	{
-			/*console.log(request.params.name);
-			console.log(request.query.answer);*/
-			/* A URL can be something like this:
-			http://www.example.com/ProblemSetName?answer=true*/
-			if (request.params[0] == "" || request.params[0] == "/")
-				console.log("index");
-
-			db.collection('math',function(err,collection){
-				collection.find({"category":request.params.name}).toArray(
-				function(err,items)
-				{
-					if (items != null)
-						response.render("app",{indata:items});
+			console.log((""+request.cookies.authkey).hashCode());
+			if ((""+request.cookies.authkey).hashCode() != 138352797)
+			{
+				response.redirect("/");
+				return;
+			}
+			else
+				db.collection('math',function(err,collection){
+					collection.find({"category":request.params.name}).toArray(
+					function(err,items)
+					{
+						if (items != null)
+							response.render("app",{indata:items});
+					});
 				});
-			});
 		});
 		app.get("/edit/:name",function(request,response)	{
-			db.collection('math',function(err,collection){
-				collection.find({"category":request.params.name}).toArray(
-				function(err,items)
-				{
-					if (items != null)
-						response.render("edit",{data:items, category:request.params.name});
+			if ((""+request.cookies.authkey).hashCode() != 138352797)
+			{
+				response.redirect("/")
+				return;
+			}
+			else
+				db.collection('math',function(err,collection){
+					collection.find({"category":request.params.name}).toArray(
+					function(err,items)
+					{
+						if (items != null)
+							response.render("edit",{data:items, category:request.params.name});
+					});
 				});
-			});
 		});
 		app.post("/admin/update/:category",function(request,response){
 			var str = request.body.data;//.replace("\\plus","+");
